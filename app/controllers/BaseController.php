68d801d6bc193ae12760e3bbfe48d088c28befed
Paul Scarrone <paul.scarrone@gmail.com>
2017-08-28 00:00:21 -0400

    These replacements for n` and n# are butchering a bunch of the api
    I don't see it being used anywhere.

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -79,19 +79,11 @@ class BaseController extends PhController
         $data = str_replace(
             [
                 '[[language]]',
-                '[[version]]',
-                '0#', '1#', '2#', '3#', '4#',
-                '5#', '6#', '7#', '8#', '9#',
-                '0`', '1`', '2`', '3`', '4`',
-                '5`', '6`', '7`', '8`', '9`',
+                '[[version]]'
             ],
             [
                 $language,
-                $this->getVersion(),
-                '#', '#', '#', '#', '#',
-                '#', '#', '#', '#', '#',
-                '`', '`', '`', '`', '`',
-                '`', '`', '`', '`', '`',
+                $this->getVersion()
             ],
             $data
         );
