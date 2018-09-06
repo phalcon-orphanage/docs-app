679c925795588f7fc3d678827c7f2f6d831ea9c6
Nikolaos Dimopoulos <nikos@niden.net>
2018-09-06 13:53:00 -0400

    Incremented the version to 3.4 (#82)

--- app/config/app.php
+++ app/config/app.php
@@ -18,7 +18,7 @@
 use function Docs\Functions\env;
 
 return [
-    'version'      => '3.3',
+    'version'      => '3.4',
     'timezone'     => env('APP_TIMEZONE', 'UTC'),
     'debug'        => env('APP_DEBUG'),
     'env'          => env('APP_ENV'),
