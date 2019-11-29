d467a5fff4d343937ebb555b5df732a11357fd55
Nikolaos Dimopoulos <nikos@niden.net>
2019-11-29 18:04:31 -0500

    Fix for the path

--- updateSponsors.php
+++ updateSponsors.php
@@ -4,9 +4,7 @@ $data = file_get_contents(
     'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
 );
 
-echo $data;
-
 file_put_contents(
-    '_includes/sponsors.html',
+    '/opt/build/repo/_includes/sponsors.html',
     $data
 );
