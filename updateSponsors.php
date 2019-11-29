7b8711dc0e54260c766ddb01cf7b71cddd11c5d7
Nikolaos Dimopoulos <nikos@niden.net>
2019-11-29 18:01:19 -0500

    Trying to figure out the download

--- updateSponsors.php
+++ updateSponsors.php
@@ -4,6 +4,8 @@ $data = file_get_contents(
     'https://raw.githubusercontent.com/phalcon/assets/master/phalcon/sponsors-fragment.html'
 );
 
+echo $data;
+
 file_put_contents(
     '_includes/sponsors.html',
     $data
