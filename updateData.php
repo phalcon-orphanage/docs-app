bcd228434ef38a3ccd1197d73a22500d149552d6
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-14 19:13:14 -0500

    Fixed update script

--- updateData.php
+++ updateData.php
@@ -1,6 +1,30 @@
 <?php
 
 $base      = "/opt/build/repo/";
+//$base      = __DIR__ . '/'; // local
+
+echo "Updating JSON" . PHP_EOL;
+$languages = array_filter(glob($base . '4.0/*'), 'is_dir');
+
+$languages = array_map(
+    function ($element) use ($base) {
+        return str_replace($base . "4.0/", "", $element);
+    },
+    $languages
+);
+
+foreach ($languages as $language) {
+    $source = $base . '4.0/' . $language . '/meta-home.json';
+    $target = $base . '_data/4-0-' . $language . '-meta-home.json';
+    copy($source, $target);
+    echo '.';
+    $source = $base . '4.0/' . $language . '/meta-topics.json';
+    $target = $base . '_data/4-0-' . $language . '-meta-topics.json';
+    copy($source, $target);
+    echo '.';
+}
+
+echo PHP_EOL;
 echo "Updating Sponsors" . PHP_EOL;
 
 $data = file_get_contents(
