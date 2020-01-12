65b1992fdece220bc7b04af8b18de499afb90afc
Nikolaos Dimopoulos <nikos@niden.net>
2020-01-11 19:55:58 -0500

    Trying the new page

--- updateData.php
+++ updateData.php
@@ -1,6 +1,8 @@
 <?php
 
 $base      = "/opt/build/repo/";
+$comments = 'https://api.github.com/repos/phalcon/cphalcon/issues/14608/comments?page=';
+
 //$base      = __DIR__ . '/'; // local
 
 echo "Updating JSON" . PHP_EOL;
@@ -57,3 +59,60 @@ file_put_contents(
     $base . '_includes/footer.html',
     $data
 );
+
+
+echo "Getting NFR Reactions" . PHP_EOL;
+
+$result = [];
+for ($i = 1; $i < 6; $i++) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $comments . $i);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Phalcon Agent');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/vnd.github.squirrel-girl-preview+json']);
+
+    $content = curl_exec($ch);
+    curl_close($ch);
+
+    echo "Got content " . $i . PHP_EOL;
+    $data = json_decode($content, true);
+
+    foreach ($data as $comment) {
+        $id        = $comment['id'] ?? '';
+        $url       = $comment['html_url'] ?? '';
+        $body      = $comment['body'] ?? '';
+        $reactions = $comment['reactions'] ?? [];
+        $plusone   = $reactions['+1'] ?? 0;
+        $body = explode(PHP_EOL, $body);
+        $body = str_replace(["\r", "\r\n"], "", $body[0]);
+
+        $plusone = substr("00" . $plusone, -3);
+        $result[$plusone . '-' . $id] = [
+            'reaction' => $plusone,
+            'body'     => "[" . $body . "](" . $url . ")",
+        ];
+    }
+}
+
+echo "Sorting Results" . PHP_EOL;
+
+krsort($result);
+
+echo "Creating Content" . PHP_EOL;
+
+$output = "| Votes  | Description             |" . PHP_EOL;
+$output .=  "|--------|-------------------------|" . PHP_EOL;
+foreach ($result as $item) {
+    $output .=  '| ' . $item['reaction'] . ' | ' . $item['body'] . ' |' . PHP_EOL;
+}
+
+$output .= PHP_EOL;
+
+foreach (glob($base . '4.0/*', GLOB_ONLYDIR) as $language) {
+    echo "Processing language " . $language . PHP_EOL;
+    $fileName = $language . '/new-feature-request-list.md';
+    $existing = file_get_contents($fileName);
+    $existing .= PHP_EOL . PHP_EOL . $output;
+    file_put_contents($fileName, $existing);
+}
